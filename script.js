0dd33794dd2c01ca1f26ee96f6cbedced221f8de
graceng-yl <yeelinng28@gmail.com>
2022-02-19 22:16:20 +0800

    combined graph, rearranged pages

--- script.js
+++ script.js
@@ -1,39 +1,46 @@
 google.charts.load('current', {packages: ['corechart']});
 google.charts.setOnLoadCallback(drawChart);
-
+// show active case
 function drawChart() {
-    var test = anarray;
-    var startdate = new Date('2021', '0', '1'); //to choose start and end range
+    var cases = graph_array;
+    var startdate = new Date('2020', '0', '1'); //to choose start and end range
 
-    for(var record=0; record<test.length; record++){
+    for(var record=0; record<cases.length; record++){
         if(record!=0){
-            for(var item=0; item<test[record].length; item++){
+            for(var item=0; item<cases[record].length; item++){
                 if(item==0){
-                    var splitted = test[record][item].split('-');
-                    test[record][item] = new Date(splitted[0], parseInt(splitted[1])-1, splitted[2]);
+                    var splitted = cases[record][item].split('-');
+                    cases[record][item] = new Date(splitted[0], parseInt(splitted[1])-1, splitted[2]);
+                    
                 }
                 else{
-                    test[record][item] = parseInt(test[record][item]);
+                    cases[record][item] = parseInt(cases[record][item]);
+                    
                 }
             }
         }
     }
-    console.log(startdate);
-    for(var record=0; record<test.length; record++){
-        if (test[record].map(Number).indexOf(+startdate) !== -1){
-            var pos = test.indexOf(test[record]);
+    //console.log(deaths);
+    for(var record=0; record<cases.length; record++){
+        if (cases[record].map(Number).indexOf(+startdate) !== -1){
+            var pos = cases.indexOf(cases[record]);
         }
     }
-    var testranged = test.slice(pos-1, test.length);
-    testranged[0] = test[0];
-    console.log(testranged);
+    var casesranged = cases.slice(pos-1, cases.length);
+    casesranged[0] = cases[0];
+    console.log(casesranged);
 
-    var data = google.visualization.arrayToDataTable(testranged);
+    var data = google.visualization.arrayToDataTable(casesranged);
 
       var options = {
         title: 'Daily Cases',
-        curveType: 'function',
-        legend: { position: 'bottom' }
+        
+        legend: { position: 'right' },
+        focusTarget: 'category',
+        height: jQuery(window).height()*1.2,
+        width: jQuery(window).width()*0.98,
+        vAxis: { viewWindow: { min:0 } },
+        backgroundColor: 'white'
       };
 
       var chart = new google.visualization.LineChart(document.getElementById('dailycases'));
