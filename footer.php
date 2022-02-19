70e9135de95efb734cb012e85d111b369569ae40
graceng-yl <yeelinng28@gmail.com>
2022-02-19 11:53:12 +0800

    init

---
+++ footer.php
@@ -0,0 +1,5 @@
+        <footer>
+            <p>Data obtained from</p>
+        </footer>
+    </body>
+</html>
---
+++ header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+
+    <body>
+        <header>
+            <nav>
+                <div><a href='index.php'>COVID-19</p></div>
+                <div>
+                    <a href='index.php'>Overview</a>
+                    <a href='cases.php'>Cases</a>
+                    <a href='deaths.php'>Deaths</a>
+                    <a href='vax.php'>Vaccination</a>
+                    <select></select>
+                </div>
+            </nav>
+        </header>
