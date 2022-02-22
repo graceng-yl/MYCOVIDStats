4194deba8c458dbf35abfd9dfd26ba8cf72fb766
graceng-yl <yeelinng28@gmail.com>
2022-02-22 19:25:44 +0800

    improved msia 1

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php 
     include('header.php'); 
     // the program only show ytd data
-    $state = ''; //to chg
+    $state = 'Johor'; //to chg
     $date = date("Y-m-d", strtotime("-1 days")); 
     
     if(!file_exists("state_cases_".$date.".csv")){
@@ -29,9 +29,9 @@
     $rowno = 0;
     $datematch = 'N';
     $vacdatematch = 'N';
-
+    $today_state_data = array();
     //retrieve yth active cases for overview
-    if($state != ''){
+    //if($state != ''){
         // foreach($state_cases as $state_cases_record) {
         //     if ($state_cases_record[0] == $date && $state_cases_record[1] == $state){
         //         $index = array_search($state_cases_record, $state_cases);
@@ -66,10 +66,13 @@
                     }
                 }
                 if($datematch=='Y' && $vacdatematch=='Y'){
-                    array_push($graph_array, [$state_case_record[0], $state_case_record[2], $state_case_record[5], $state_case_record[4], $state_death_record[2], $state_vac_record[5]]);
+                    //if($state_case_record[1]==$state){
+                        array_push($graph_array, [$state_case_record[0], $state_case_record[2], $state_case_record[5], $state_case_record[4], $state_death_record[2], $state_vac_record[5]]);
+                    //}
                     if($state_case_record[0] == $date){
                         $today_data = [$state_case_record[2], $state_case_record[3], $state_case_record[5], $state_case_record[4], $state_death_record[2], $state_vac_record[5]];
                         //cases_new,cases_import,cases_active,cases_recovered,deaths_new,daily
+                        //get the same date all state record, state_cases[index-1...][2...]
                     }
                 }elseif($datematch=='Y' && $vacdatematch=='N'){
                     array_push($graph_array, [$state_case_record[0], $state_case_record[2], $state_case_record[5], $state_case_record[4], $state_death_record[2], 0]);
@@ -87,13 +90,22 @@
                         $today_data = [$state_case_record[2], $state_case_record[3], $state_case_record[5], $state_case_record[4], 0, 0];
                     }
                 }
+                // if($datematch=='Y' && $vacdatematch=='Y' && $state_case_record[0] == $date){
+                    //array_push($today_state_data, [$state_case_record[1], $state_case_record[2], $state_case_record[3], $state_case_record[5], $state_case_record[4], $state_death_record[2], $state_vac_record[5]]);
+                //     //state,cases_new,cases_import,cases_active,cases_recovered,deaths_new,daily
+                // }elseif($datematch=='Y' && $vacdatematch=='N' && $state_case_record[0] == $date){
+                //     array_push($today_state_data, [$state_case_record[1], $state_case_record[2], $state_case_record[3], $state_case_record[5], $state_case_record[4], $state_death_record[2], 0]);
+                // }elseif($datematch=='N' && $vacdatematch=='Y' && $state_case_record[0] == $date){
+                //     array_push($today_state_data, [$state_case_record[1], $state_case_record[2], $state_case_record[3], $state_case_record[5], $state_case_record[4], 0, $state_vac_record[5]]);
+                // }elseif($datematch=='N' && $vacdatematch=='N' && $state_case_record[0] == $date){
+                //     array_push($today_state_data, [$state_case_record[1], $state_case_record[2], $state_case_record[3], $state_case_record[5], $state_case_record[4], 0, 0]);
+                // }
             }
         }
-    }
+   // }
 
 
-    else{
-        $today_state_data = array();
+    //else{
         // foreach($msia_cases as $cases_record) {
         //     if (strpos($cases_record, $date) !== false){
         //         $rownondex = array_search($cases_record, $msia_cases);
@@ -102,6 +114,8 @@
         // }
         // $cases_yth = explode(',', $cases_record_ytd)[4];
 
+
+////put if state '' here
         foreach ($msia_cases as $case_record){ 
             if($rowno==0){ //if first row
                 array_push($graph_array, ['Date', 'New Cases', 'Active Cases', 'Recovered', 'Deaths', 'Vaccines Administered']);
@@ -127,23 +141,23 @@
                         $vacdatematch = 'N';
                     }
                 }
-                if($datematch=='Y' && $vacdatematch=='Y'){
+                if($datematch=='Y' && $vacdatematch=='Y' && $state == ''){
                     array_push($graph_array, [$case_record[0], $case_record[1], $case_record[4], $case_record[3], $death_record[1], $vac_record[4]]);
                     if($case_record[0] == $date){
                         $today_data = [$case_record[1], $case_record[2], $case_record[4], $case_record[3], $death_record[1], $vac_record[4]];
                         //cases_new,cases_import,cases_active,cases_recovered,deaths_new,daily
                     }
-                }elseif($datematch=='Y' && $vacdatematch=='N'){
+                }elseif($datematch=='Y' && $vacdatematch=='N' && $state == ''){
                     array_push($graph_array, [$case_record[0], $case_record[1], $case_record[4], $case_record[3], $death_record[1], 0]);
                     if($case_record[0] == $date){
                         $today_data = [$case_record[1], $case_record[2], $case_record[4], $case_record[3], $death_record[1], 0];
                     }
-                }elseif($datematch=='N' && $vacdatematch=='Y'){
+                }elseif($datematch=='N' && $vacdatematch=='Y' && $state == ''){
                     array_push($graph_array, [$case_record[0], $case_record[1], $case_record[4], $case_record[3], 0, $vac_record[4]]);
                     if($case_record[0] == $date){
                         $today_data = [$case_record[1], $case_record[2], $case_record[4], $case_record[3], 0, $vac_record[4]];
                     }
-                }else{
+                }elseif($datematch=='N' && $vacdatematch=='Y' && $state == ''){
                     array_push($graph_array, [$case_record[0], $case_record[1], $case_record[4], $case_record[3], 0, 0]);
                     if($case_record[0] == $date){
                         $today_data = [$case_record[1], $case_record[2], $case_record[4], $case_record[3], 0, 0];
@@ -153,47 +167,47 @@
         }
 
 
-        foreach ($state_cases as $state_case_record){ 
-            // if($rowno==0){ //if first row
-            //     array_push($graph_array, ['Date', 'New Cases', 'Active Cases', 'Recovered', 'Deaths', 'Vaccines Administered']);
-            //     $rowno = 1; //done, set to any other value so that the data rows use elseif
-            // }
-            // elseif ($state_case_record != '' && strpos($state_case_record, $state)!==false){
-                //$state_case_record = explode(',', $state_case_record);
-                if(isset($state_case_record[1])){
-                    $curr_state = $state_case_record[1];
-                }
-                foreach($state_deaths as $state_death_record) { //compare with death csv, if date different set 0 death
-                    if ($state_death_record[0] == $state_case_record[0] && $state_death_record[1] == $curr_state){
-                        $datematch = 'Y'; 
-                        //$state_death_record = explode(',', $state_death_record);
-                        break;
-                    }else{
-                        $state_datematch = 'N';
-                    }
-                }
-                foreach($state_vac as $state_vac_record){ //compare with vac csv, if date different set 0 vac
-                    if ($state_vac_record[0] == $state_case_record[0] && $state_vac_record[1] == $curr_state){
-                        $vacdatematch = 'Y'; 
-                        //$state_vac_record = explode(',', $state_vac_record);
-                        break;
-                    }else{
-                        $vacdatematch = 'N';
-                    }
-                }
-                if($datematch=='Y' && $vacdatematch=='Y' && $state_case_record[0] == $date){
-                    array_push($today_state_data, [$state_case_record[1], $state_case_record[2], $state_case_record[3], $state_case_record[5], $state_case_record[4], $state_death_record[2], $state_vac_record[5]]);
-                    //state,cases_new,cases_import,cases_active,cases_recovered,deaths_new,daily
-                }elseif($datematch=='Y' && $vacdatematch=='N' && $state_case_record[0] == $date){
-                    array_push($today_state_data, [$state_case_record[1], $state_case_record[2], $state_case_record[3], $state_case_record[5], $state_case_record[4], $state_death_record[2], 0]);
-                }elseif($datematch=='N' && $vacdatematch=='Y' && $state_case_record[0] == $date){
-                    array_push($today_state_data, [$state_case_record[1], $state_case_record[2], $state_case_record[3], $state_case_record[5], $state_case_record[4], 0, $state_vac_record[5]]);
-                }elseif($datematch=='N' && $vacdatematch=='N' && $state_case_record[0] == $date){
-                    array_push($today_state_data, [$state_case_record[1], $state_case_record[2], $state_case_record[3], $state_case_record[5], $state_case_record[4], 0, 0]);
-                }
-           // }
-        }
-    }
+        // foreach ($state_cases as $state_case_record){ 
+        //     // if($rowno==0){ //if first row
+        //     //     array_push($graph_array, ['Date', 'New Cases', 'Active Cases', 'Recovered', 'Deaths', 'Vaccines Administered']);
+        //     //     $rowno = 1; //done, set to any other value so that the data rows use elseif
+        //     // }
+        //     // elseif ($state_case_record != '' && strpos($state_case_record, $state)!==false){
+        //         //$state_case_record = explode(',', $state_case_record);
+        //         if(isset($state_case_record[1])){
+        //             $curr_state = $state_case_record[1];
+        //         }
+        //         foreach($state_deaths as $state_death_record) { //compare with death csv, if date different set 0 death
+        //             if ($state_death_record[0] == $state_case_record[0] && $state_death_record[1] == $curr_state){
+        //                 $datematch = 'Y'; 
+        //                 //$state_death_record = explode(',', $state_death_record);
+        //                 break;
+        //             }else{
+        //                 $state_datematch = 'N';
+        //             }
+        //         }
+        //         foreach($state_vac as $state_vac_record){ //compare with vac csv, if date different set 0 vac
+        //             if ($state_vac_record[0] == $state_case_record[0] && $state_vac_record[1] == $curr_state){
+        //                 $vacdatematch = 'Y'; 
+        //                 //$state_vac_record = explode(',', $state_vac_record);
+        //                 break;
+        //             }else{
+        //                 $vacdatematch = 'N';
+        //             }
+        //         }
+        //         if($datematch=='Y' && $vacdatematch=='Y' && $state_case_record[0] == $date){
+        //             array_push($today_state_data, [$state_case_record[1], $state_case_record[2], $state_case_record[3], $state_case_record[5], $state_case_record[4], $state_death_record[2], $state_vac_record[5]]);
+        //             //state,cases_new,cases_import,cases_active,cases_recovered,deaths_new,daily
+        //         }elseif($datematch=='Y' && $vacdatematch=='N' && $state_case_record[0] == $date){
+        //             array_push($today_state_data, [$state_case_record[1], $state_case_record[2], $state_case_record[3], $state_case_record[5], $state_case_record[4], $state_death_record[2], 0]);
+        //         }elseif($datematch=='N' && $vacdatematch=='Y' && $state_case_record[0] == $date){
+        //             array_push($today_state_data, [$state_case_record[1], $state_case_record[2], $state_case_record[3], $state_case_record[5], $state_case_record[4], 0, $state_vac_record[5]]);
+        //         }elseif($datematch=='N' && $vacdatematch=='N' && $state_case_record[0] == $date){
+        //             array_push($today_state_data, [$state_case_record[1], $state_case_record[2], $state_case_record[3], $state_case_record[5], $state_case_record[4], 0, 0]);
+        //         }
+        //    // }
+        // }
+    //}
     
 
 ?>
