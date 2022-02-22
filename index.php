dc0f42d3441517282d009d2c79dcdab88533b92e
graceng-yl <yeelinng28@gmail.com>
2022-02-22 23:54:16 +0800

    date filter

--- index.php
+++ index.php
@@ -1,191 +1,10 @@
 <?php 
     include('header.php'); 
-    // the program only show ytd data
-    $state = '';
-    if(isset($_GET['selectedstate'])){
-        $state = $_GET['selectedstate']; 
-    }
-    $date = date("Y-m-d", strtotime("-1 days")); 
-    $ytddate = date("Y-m-d", strtotime("-2 days")); 
-
-    //delete old files
-    if(file_exists("state_cases_".$ytddate.".csv")){
-        unlink("state_cases_".$ytddate.".csv");
-        unlink("state_deaths_".$ytddate.".csv");
-        unlink("state_vac_".$ytddate.".csv");
-        unlink("msia_cases_".$ytddate.".csv");
-        unlink("msia_deaths_".$ytddate.".csv");
-        unlink("msia_vac_".$ytddate.".csv");
-    }
-
-    //dl new file if not yet dl
-    if(!file_exists("state_cases_".$date.".csv")){
-        file_put_contents("state_cases_".$date.".csv", file_get_contents("https://raw.githubusercontent.com/MoH-Malaysia/covid19-public/main/epidemic/cases_state.csv"));
-    }if(!file_exists("state_deaths_".$date.".csv")){
-        file_put_contents("state_deaths_".$date.".csv", file_get_contents("https://raw.githubusercontent.com/MoH-Malaysia/covid19-public/main/epidemic/deaths_state.csv"));
-    }if(!file_exists("state_vac_".$date.".csv")){
-        file_put_contents("state_vac_".$date.".csv", file_get_contents("https://raw.githubusercontent.com/MoH-Malaysia/covid19-public/main/vaccination/vax_state.csv"));
-    }if(!file_exists("msia_cases_".$date.".csv")){
-        file_put_contents("msia_cases_".$date.".csv", file_get_contents("https://raw.githubusercontent.com/MoH-Malaysia/covid19-public/main/epidemic/cases_malaysia.csv"));
-    }if(!file_exists("msia_deaths_".$date.".csv")){
-        file_put_contents("msia_deaths_".$date.".csv", file_get_contents("https://raw.githubusercontent.com/MoH-Malaysia/covid19-public/main/epidemic/deaths_malaysia.csv"));
-    }if(!file_exists("msia_vac_".$date.".csv")){
-        file_put_contents("msia_vac_".$date.".csv", file_get_contents("https://raw.githubusercontent.com/MoH-Malaysia/covid19-public/main/vaccination/vax_malaysia.csv"));
-    }
-    
-    $state_cases = array_map('str_getcsv', file("state_cases_".$date.".csv"));
-    $state_deaths = array_map('str_getcsv', file("state_deaths_".$date.".csv"));
-    $state_vac = array_map('str_getcsv', file("state_vac_".$date.".csv"));
-    $msia_cases = array_map('str_getcsv', file("msia_cases_".$date.".csv"));
-    $msia_deaths = array_map('str_getcsv', file("msia_deaths_".$date.".csv"));
-    $msia_vac = array_map('str_getcsv', file("msia_vac_".$date.".csv"));
-
-    $graph_array = array(); 
-    $rowno = 0;
-    // $datematch = 'N';
-    // $vacdatematch = 'N';
-    $today_state_data = array();
-
-    //main page
-    if($state == ''){
-        //today data
-        foreach ($msia_cases as $case_record){ 
-            if($rowno==0){ //if first row
-                array_push($graph_array, ['Date', 'New Cases', 'Active Cases', 'Recovered', 'Deaths', 'Vaccines Administered']);
-                $rowno = 1; //done, set to any other value so that the data rows use elseif
-            }
-            else{
-                if ($case_record[0] == $ytddate){ //ytd data
-                    $cases_ytd = $case_record[4];
-                }
-                foreach($msia_deaths as $death_record) { //compare with death csv, if date different set 0 death
-                    if ($death_record[0] == $case_record[0]){
-                        $datematch = 'Y'; 
-                        break;
-                    }else{
-                        $datematch = 'N';
-                    }
-                }
-                foreach($msia_vac as $vac_record){ //compare with vac csv, if date different set 0 vac
-                    if ($vac_record[0] == $case_record[0]){
-                        $vacdatematch = 'Y'; 
-                        break;
-                    }else{
-                        $vacdatematch = 'N';
-                    }
-                }
-                if($datematch=='Y' && $vacdatematch=='Y' && $state == ''){
-                    array_push($graph_array, [$case_record[0], $case_record[1], $case_record[4], $case_record[3], $death_record[1], $vac_record[4]]);
-                    if($case_record[0] == $date){
-                        $today_data = [$case_record[1], $case_record[2], $case_record[4], $case_record[3], $death_record[1], $vac_record[4]];
-                        //cases_new,cases_import,cases_active,cases_recovered,deaths_new,daily
-                    }
-                }elseif($datematch=='Y' && $vacdatematch=='N' && $state == ''){
-                    array_push($graph_array, [$case_record[0], $case_record[1], $case_record[4], $case_record[3], $death_record[1], 0]);
-                    if($case_record[0] == $date){
-                        $today_data = [$case_record[1], $case_record[2], $case_record[4], $case_record[3], $death_record[1], 0];
-                    }
-                }elseif($datematch=='N' && $vacdatematch=='Y' && $state == ''){
-                    array_push($graph_array, [$case_record[0], $case_record[1], $case_record[4], $case_record[3], 0, $vac_record[4]]);
-                    if($case_record[0] == $date){
-                        $today_data = [$case_record[1], $case_record[2], $case_record[4], $case_record[3], 0, $vac_record[4]];
-                    }
-                }elseif($datematch=='N' && $vacdatematch=='Y' && $state == ''){
-                    array_push($graph_array, [$case_record[0], $case_record[1], $case_record[4], $case_record[3], 0, 0]);
-                    if($case_record[0] == $date){
-                        $today_data = [$case_record[1], $case_record[2], $case_record[4], $case_record[3], 0, 0];
-                    }
-                }
-            }
-        }
-
-        //table data
-        $i = 0;
-        foreach($state_cases as $state_cases_record1){
-            if($state_cases_record1[0]==$date){
-                $today_state_data[$i] = [$state_cases_record1[1], $state_cases_record1[2], $state_cases_record1[3], $state_cases_record1[5], $state_cases_record1[4]];
-                $i++;
-            }
-            
-        }
-        $i = 0;
-        foreach($state_deaths as $state_deaths_record1){
-            if($state_deaths_record1[0]==$date){
-                array_push($today_state_data[$i], $state_deaths_record1[2]);
-                $i++;
-            }
-        }
-        $i = 0;
-        foreach($state_vac as $state_vac_record1){
-            if($state_vac_record1[0]==$date){
-                array_push($today_state_data[$i], $state_vac_record1[4]);
-                $i++;
-            }
-        }
-    }
-
-    //state data
-    else{        
-        //today data
-        foreach ($state_cases as $state_case_record){ 
-            if($rowno==0){ //if first row
-                array_push($graph_array, ['Date', 'New Cases', 'Active Cases', 'Recovered', 'Deaths', 'Vaccines Administered']);
-                $rowno = 1; //done, set to any other value so that the data rows use elseif
-            }
-            elseif ($state_case_record[1] == $state){
-                if ($state_case_record[0] == $ytddate && $state_case_record[1] == $state){ //ytd data
-                    $cases_ytd = $state_case_record[5];
-                }
-                //$state_case_record = explode(',', $state_case_record);
-                foreach($state_deaths as $state_death_record) { //compare with death csv, if date different set 0 death
-                    if ($state_death_record[0] == $state_case_record[0] && $state_death_record[1] == $state){
-                        $datematch = 'Y'; 
-                        break;
-                    }else{
-                        $datematch = 'N';
-                    }
-                }
-                foreach($state_vac as $state_vac_record){ //compare with vac csv, if date different set 0 vac
-                    if ($state_vac_record[0] == $state_case_record[0] && $state_vac_record[1] == $state){
-                        $vacdatematch = 'Y'; 
-                        break;
-                    }else{
-                        $vacdatematch = 'N';
-                    }
-                }
-                
-                if($datematch=='Y' && $vacdatematch=='Y'){
-                    array_push($graph_array, [$state_case_record[0], $state_case_record[2], $state_case_record[5], $state_case_record[4], $state_death_record[2], $state_vac_record[5]]);
-                    if($state_case_record[0] == $date){
-                        $today_data = [$state_case_record[2], $state_case_record[3], $state_case_record[5], $state_case_record[4], $state_death_record[2], $state_vac_record[5]];
-                        //cases_new,cases_import,cases_active,cases_recovered,deaths_new,daily
-                    }
-                }elseif($datematch=='Y' && $vacdatematch=='N'){
-                    array_push($graph_array, [$state_case_record[0], $state_case_record[2], $state_case_record[5], $state_case_record[4], $state_death_record[2], 0]);
-                    if($state_case_record[0] == $date){
-                        $today_data = [$state_case_record[2], $state_case_record[3], $state_case_record[5], $state_case_record[4], $state_death_record[2], 0];
-                    }
-                }elseif($datematch=='N' && $vacdatematch=='Y'){
-                    array_push($graph_array, [$state_case_record[0], $state_case_record[2], $state_case_record[5], $state_case_record[4], 0, $state_vac_record[5]]);
-                    if($state_case_record[0] == $date){
-                        $today_data = [$state_case_record[2], $state_case_record[3], $state_case_record[5], $state_case_record[4], 0, $state_vac_record[5]];
-                    }
-                }else{
-                    array_push($graph_array, [$state_case_record[0], $state_case_record[2], $state_case_record[5], $state_case_record[4], 0, 0]);
-                    if($state_case_record[0] == $date){
-                        $today_data = [$state_case_record[2], $state_case_record[3], $state_case_record[5], $state_case_record[4], 0, 0];
-                    }
-                }
-            }
-        }
-    }
-    
-
+    include('data.php');
 ?>
     <script>var graph_array=<?php echo json_encode($graph_array); ?>;</script>
     <p id='state' title='<?php echo $state; ?>'><?php echo $state; ?></p>
     <div><?php echo $date; ?></div>
-
 
     <div>
         <b>Cases</b>
@@ -252,8 +71,13 @@
         </table>
 <?php
     }
-?>
-    <div id="dailycases"></div>
+?>  
+    <form id=dateselector>
+        <input type="date" id='startdate'>
+        <input type="date" id='enddate'>
+        <div id='filter'>Filter</div>
+    </form>
+    <div id="trendgraph"></div>
 
 <?php
     include('footer.php'); 
--- script.js
+++ script.js
@@ -11,20 +11,66 @@ jQuery(document).ready(function() {
         }
     });
 
+    google.charts.load('current', {packages: ['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+    //to choose start and end range
+    document.getElementById('filter').addEventListener("click", function(){
+        startdate = document.getElementById('startdate').value; 
+        enddate = document.getElementById('enddate').value;
+        var splitted_startdate = startdate.split('-');
+        var splitted_enddate = enddate.split('-');
+        startdate = new Date(splitted_startdate[0], parseInt(splitted_startdate[1])-1, splitted_startdate[2]);
+        enddate = new Date(splitted_enddate[0], parseInt(splitted_enddate[1])-1, splitted_enddate[2]);
+        //console.log(new Date(splitted_enddate[0], parseInt(splitted_enddate[1])-1, splitted_enddate[2]));
+        //drawChart(startdate, enddate);
+        drawChart();
+    });
 });
+
+
 
 
 google.charts.load('current', {packages: ['corechart']});
 google.charts.setOnLoadCallback(drawChart);
 
-function drawChart() {
-    var cases = graph_array;
-    var startdate = new Date('2020', '0', '1'); //to choose start and end range
 
+function addrows(start, end, data){
+    //console.log(start-1);
+    console.log(start, end, data);
+    var length = data.getNumberOfRows()-end;
+    data.removeRows(0, start-1);
+    data.removeRows(end, length);
+    //data = data.removeRows(end+1, length-end+1);
+
+    // for (var i = start-1; i == 0; i--) {
+    //   data = data.removeRow(i);
+    // }
+    // for (var i = end; i < data.getNumberOfRows(); i++){
+    //   console.log('hi');
+    //   data = data.removeRow(i);
+    // }
+    //console.log(data.getNumberOfRows());
+    return data;
+}
+
+function drawChart() {
+    console.log(graph_array);
+    var cases = Array.from(graph_array); //
+    var d = new Date();
+    var startdate = new Date(d.setDate(d.getDate() - 31));
+    //console.log(startdate);
+    startdate = startdate.setHours(0,0,0,0);
+    //console.log(startdate);
+    var d = new Date();
+    var enddate = new Date(d.setDate(d.getDate() - 1));
+    enddate = enddate.setHours(0,0,0,0);
+    
     for(var record=0; record<cases.length; record++){
         if(record!=0){
             for(var item=0; item<cases[record].length; item++){
                 if(item==0){
+                    console.log(cases[record][item]);
                     var splitted = cases[record][item].split('-');
                     cases[record][item] = new Date(splitted[0], parseInt(splitted[1])-1, splitted[2]);
                 }
@@ -34,25 +80,36 @@ function drawChart() {
             }
         }
     }
+    var startpos = 0;
+    var endpos = 0;
     for(var record=0; record<cases.length; record++){
+        //console.log(startdate); //console.log(cases[record]);
         if (cases[record].map(Number).indexOf(+startdate) !== -1){
-            var pos = cases.indexOf(cases[record]);
+            //console.log(cases[record]);
+            startpos = cases.indexOf(cases[record]);
+        }
+        if (cases[record].map(Number).indexOf(+enddate) !== -1){
+            //console.log(cases[record]);
+            endpos = cases.indexOf(cases[record]);
         }
     }
-    var casesranged = cases.slice(pos-1, cases.length);
-    casesranged[0] = cases[0];
+    // var casesranged = cases.slice(startpos, endpos);
+    // casesranged[0] = cases[0];
 
-    var data = google.visualization.arrayToDataTable(casesranged);
+    var data = google.visualization.arrayToDataTable(cases);
+    //console.log(startpos, endpos, data);
+    data = addrows(startpos, endpos, data);
     var options = {
-        title: 'Daily Cases',
+        title: '',
         legend: { position: 'right' },
         focusTarget: 'category',
-        height: jQuery(window).height()*1.2,
-        width: jQuery(window).width()*0.98,
-        vAxis: { viewWindow: { min:0 } },
+        //height: jQuery(window).height()*1.2,
+        //width: jQuery(window).width()*0.98,
+        vAxis: { title: 'Number', viewWindow: { min:0 } },
+        hAxis: { title: 'Date' },
         backgroundColor: 'white'
     };
-    var chart = new google.visualization.LineChart(document.getElementById('dailycases'));
+    var chart = new google.visualization.LineChart(document.getElementById('trendgraph'));
     chart.draw(data, options);
 
     var columns = [];
