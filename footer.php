e1c93a22f7c1a1fb0a277cf4738f6c3143b8f634
graceng-yl <yeelinng28@gmail.com>
2022-02-22 22:11:07 +0800

    graph column selection

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
         <footer>
-            <p>Data obtained from</p>
+            <p>Data obtained from <a href="https://github.com/MoH-Malaysia/covid19-public/tree/main/">Ministry of Health Malaysia</a></p>
         </footer>
     </body>
 </html>
--- script.js
+++ script.js
@@ -1,55 +1,3 @@
-google.charts.load('current', {packages: ['corechart']});
-google.charts.setOnLoadCallback(drawChart);
-// show active case
-function drawChart() {
-    var cases = graph_array;
-    var startdate = new Date('2020', '0', '1'); //to choose start and end range
-
-    for(var record=0; record<cases.length; record++){
-        if(record!=0){
-            for(var item=0; item<cases[record].length; item++){
-                if(item==0){
-                    var splitted = cases[record][item].split('-');
-                    cases[record][item] = new Date(splitted[0], parseInt(splitted[1])-1, splitted[2]);
-                    
-                }
-                else{
-                    cases[record][item] = parseInt(cases[record][item]);
-                    
-                }
-            }
-        }
-    }
-    //console.log(deaths);
-    for(var record=0; record<cases.length; record++){
-        if (cases[record].map(Number).indexOf(+startdate) !== -1){
-            var pos = cases.indexOf(cases[record]);
-        }
-    }
-    var casesranged = cases.slice(pos-1, cases.length);
-    casesranged[0] = cases[0];
-    //console.log(casesranged);
-
-    var data = google.visualization.arrayToDataTable(casesranged);
-
-      var options = {
-        title: 'Daily Cases',
-        
-        legend: { position: 'right' },
-        focusTarget: 'category',
-        height: jQuery(window).height()*1.2,
-        width: jQuery(window).width()*0.98,
-        vAxis: { viewWindow: { min:0 } },
-        backgroundColor: 'white'
-      };
-
-      var chart = new google.visualization.LineChart(document.getElementById('dailycases'));
-
-      chart.draw(data, options);
-  }
-
-
-
 jQuery(document).ready(function() {
 	document.getElementById('statedropdown').addEventListener("change", function(){
         //console.log('hello');
@@ -64,3 +12,85 @@ jQuery(document).ready(function() {
     });
 
 });
+
+
+google.charts.load('current', {packages: ['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+    var cases = graph_array;
+    var startdate = new Date('2020', '0', '1'); //to choose start and end range
+
+    for(var record=0; record<cases.length; record++){
+        if(record!=0){
+            for(var item=0; item<cases[record].length; item++){
+                if(item==0){
+                    var splitted = cases[record][item].split('-');
+                    cases[record][item] = new Date(splitted[0], parseInt(splitted[1])-1, splitted[2]);
+                }
+                else{
+                    cases[record][item] = parseInt(cases[record][item]);
+                }
+            }
+        }
+    }
+    for(var record=0; record<cases.length; record++){
+        if (cases[record].map(Number).indexOf(+startdate) !== -1){
+            var pos = cases.indexOf(cases[record]);
+        }
+    }
+    var casesranged = cases.slice(pos-1, cases.length);
+    casesranged[0] = cases[0];
+
+    var data = google.visualization.arrayToDataTable(casesranged);
+    var options = {
+        title: 'Daily Cases',
+        legend: { position: 'right' },
+        focusTarget: 'category',
+        height: jQuery(window).height()*1.2,
+        width: jQuery(window).width()*0.98,
+        vAxis: { viewWindow: { min:0 } },
+        backgroundColor: 'white'
+    };
+    var chart = new google.visualization.LineChart(document.getElementById('dailycases'));
+    chart.draw(data, options);
+
+    var columns = [];
+    var series = {};
+    for (var i = 0; i < data.getNumberOfColumns(); i++) {
+        columns.push(i);
+        if (i > 0) {
+            series[i - 1] = {};
+        }
+    }
+
+    google.visualization.events.addListener(chart, 'select', function () {
+        var sel = chart.getSelection();
+        // if selection length is 0, we deselected an element
+        if (sel.length > 0) {
+          // if row is undefined, we clicked on the legend
+            if (sel[0].row === null) {
+                var col = sel[0].column;
+                if (columns[col] == col) {
+                // hide the data series
+                    columns[col] = {
+                        label: data.getColumnLabel(col),
+                        type: data.getColumnType(col),
+                        calc: function () {
+                        return null;
+                    },
+                };
+                // grey out the legend entry
+                series[col - 1].color = '#CCCCCC';
+                } else {
+                    // show the data series
+                    columns[col] = col;
+                    series[col - 1].color = null;
+                }
+                var view = new google.visualization.DataView(data);
+                view.setColumns(columns);
+                chart.draw(view, options);
+            }
+        }
+    });
+}
